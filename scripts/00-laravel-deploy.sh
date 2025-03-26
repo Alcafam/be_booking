2675d9dad6006b8326863fb1473e18b676da9473
AlcaFam <galinganmarjy61@gmail.com>
2025-03-27 04:24:52 +0800

    Final Fix pero d pa linis3

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+# echo "Running composer"
+# composer global require hirak/prestissimo
+# composer install --no-dev --working-dir=/var/www/html
 
 # echo "generating application key..."
 # php artisan key:generate --show
@@ -12,12 +12,11 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-
 echo "Running migration reset"
 php artisan migrate:fresh
 
-echo "Running migrations..."
-php artisan migrate --force
+# echo "Running migrations..."
+# php artisan migrate --force
 
 echo "Running seeders..."
 php artisan db:seed
