0d461112493c1b57a8e33b690fbc3a4dc33dca15
AlcaFam <galinganmarjy61@gmail.com>
2025-03-27 06:30:28 +0800

    aaaaa3

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,8 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+# echo "generating application key..."
+# php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -16,14 +16,12 @@ php artisan route:cache
 # php artisan migrate:reset
 
 echo "Running migrations..."
-# php artisan migrate --path=/database/migrations/2014_10_12_000000_create_users_table.php
-# php artisan migrate --path=/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
-# php artisan migrate --path=/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
-# php artisan migrate --path=/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
-# php artisan migrate --path=/database/migrations/2025_03_21_195706_create_events_table.php
-# php artisan migrate --path=/database/migrations/2025_03_21_195726_create_bookings_table.php
-
-php artisan migrate
+php artisan migrate --path=/database/migrations/2014_10_12_000000_create_users_table.php --force
+php artisan migrate --path=/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php --force
+php artisan migrate --path=/database/migrations/2019_08_19_000000_create_failed_jobs_table.php --force
+php artisan migrate --path=/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php --force
+php artisan migrate --path=/database/migrations/2025_03_21_195706_create_events_table.php --force
+php artisan migrate --path=/database/migrations/2025_03_21_195726_create_bookings_table.php --force
 
 echo "Running seeders..."
 php artisan db:seed
