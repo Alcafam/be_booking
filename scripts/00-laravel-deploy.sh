1c930c9046101e760554e4d102ec9cdf3a251aa3
AlcaFam <galinganmarjy61@gmail.com>
2025-03-27 06:10:26 +0800

    aaaaa2

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -12,16 +12,16 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migration reset"
-php artisan migrate:reset
+# echo "Running migration reset"
+# php artisan migrate:reset
 
-echo "Running migrations..."
-php artisan migrate --path=/database/migrations/2014_10_12_000000_create_users_table.php
-php artisan migrate --path=/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
-php artisan migrate --path=/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
-php artisan migrate --path=/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
-php artisan migrate --path=/database/migrations/2025_03_21_195706_create_events_table.php
-php artisan migrate --path=/database/migrations/2025_03_21_195726_create_bookings_table.php
+# echo "Running migrations..."
+# php artisan migrate --path=/database/migrations/2014_10_12_000000_create_users_table.php
+# php artisan migrate --path=/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+# php artisan migrate --path=/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+# php artisan migrate --path=/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+# php artisan migrate --path=/database/migrations/2025_03_21_195706_create_events_table.php
+# php artisan migrate --path=/database/migrations/2025_03_21_195726_create_bookings_table.php
 
 echo "Running seeders..."
 php artisan db:seed
