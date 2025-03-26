27607ce926960f40289dd260d75235a1b6fe1f32
AlcaFam <galinganmarjy61@gmail.com>
2025-03-27 06:49:18 +0800

    aaaaa4

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -15,20 +15,20 @@ php artisan route:cache
 # echo "Running migration reset"
 # php artisan migrate:reset
 
-echo "Running migrations..."
-php artisan migrate --path=/database/migrations/2014_10_12_000000_create_users_table.php --force
-php artisan migrate --path=/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php --force
-php artisan migrate --path=/database/migrations/2019_08_19_000000_create_failed_jobs_table.php --force
-php artisan migrate --path=/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php --force
-php artisan migrate --path=/database/migrations/2025_03_21_195706_create_events_table.php --force
-php artisan migrate --path=/database/migrations/2025_03_21_195726_create_bookings_table.php --force
+# echo "Running migrations..."
+# php artisan migrate --path=/database/migrations/2014_10_12_000000_create_users_table.php --force
+# php artisan migrate --path=/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php --force
+# php artisan migrate --path=/database/migrations/2019_08_19_000000_create_failed_jobs_table.php --force
+# php artisan migrate --path=/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php --force
+# php artisan migrate --path=/database/migrations/2025_03_21_195706_create_events_table.php --force
+# php artisan migrate --path=/database/migrations/2025_03_21_195726_create_bookings_table.php --force
 
-echo "Running seeders..."
-php artisan db:seed --force
+# echo "Running seeders..."
+# php artisan db:seed --force
 
 # Check if the seed command was successful
-if [ $? -eq 0 ]; then
-    echo "Seeders ran successfully."
-else
-    echo "There was an error running the seeders."
-fi
+# if [ $? -eq 0 ]; then
+#     echo "Seeders ran successfully."
+# else
+#     echo "There was an error running the seeders."
+# fi
